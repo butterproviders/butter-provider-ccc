378a40e48abb37180868a3694900d922d87e775c
Niv Sardi <xaiki@evilgiggle.com>
2018-03-29 17:40:35 -0300

    update to ES6
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- index.js
+++ index.js
@@ -1,169 +1,103 @@
 'use strict';
 
-var Provider = require('butter-provider');
-var querystring = require('querystring');
-var deferRequest = require('defer-request');
-var inherits = require('util').inherits;
-var moment = require('moment');
+var Provider = require('butter-provider')
+var moment = require('moment')
+var axios = require('axios')
+var debug = require('debug')('butter-provider-ccc')
 
-var CCC = function () {
-    CCC.super_.apply(this, arguments);
-    this.URL = this.args.urlList[0];
-};
-
-inherits(CCC, Provider);
-
-CCC.prototype.config = {
+const defaultConfig = {
     name: 'ccc',
     uniqueId: 'imdb_id',
     tabName: 'CCC',
     defaults: {
         urlList: ['https://media.ccc.de/public'],
-        formats: ['video/webm', 'video/mp4']
+        formats: [
+            'video/webm',
+            'video/mp4'
+        ],
+        timeout: 40000,
+        limit: 10
     },
-    args: {
+    argTypes: {
         urlList: Provider.ArgType.ARRAY,
         langs: Provider.ArgType.ARRAY,
-        formats: Provider.ArgType.ARRAY
+        formats: Provider.ArgType.ARRAY,
+        timeout: Provider.ArgType.NUMBER,
+        limit: Provider.ArgType.NUMBER
     }
-};
-
-CCC.prototype._queryTorrents = function (filters) {
-    var params = {};
-    var genres = '';
-    params.sort = 'seeds';
-    params.limit = '50';
-
-    if (filters.keywords) {
-        params.keywords = filters.keywords.replace(/\s/g, '% ');
-    }
-
-    if (filters.genre) {
-        //            filters.genres.forEach(function(g) {
-        //                genres += '&genre[]='+g;
-        //            });
-        //            genres = genres.substring(0, genres.length - 1);
-        //            win.info('genres', genres);
-        params.genre = filters.genres[0];
-    }
-
-    if (filters.order) {
-        params.order = filters.order;
-    }
-
-    if (filters.sorter && filters.sorter !== 'popularity') {
-        params.sort = filters.sorter;
-    }
-
-    return deferRequest(this.URL + '/conferences')
-        .then(function (data) {
-            return data.conferences
-        })
-        .catch(function (err) {
-            console.error ('CCC', 'error', err)
-        })
-};
-
-var formatElementForButter = function (data) {
-    var langs = this.args.langs;
-
-    var id = data.url.split('/').pop();
-    var updated = moment(data.updated_at);
-    var year = updated.year();
-    var img = data.logo_url;
-    return deferRequest(data.url)
-        .then(function (conf) {
-            var days = {}
-
-            var events =
-                conf.events
-                    .filter(function(event) {
-                        return langs?(langs.indexOf(event.original_language) != -1):true;
-                    }).map(function (event) {
-                        var day = getEventDate(event);
-                        days[day] = 1;
-                        return Object.assign({}, event, {
-                            first_aired: event.date ? moment(event.date).unix() : 0,
-                            day: day
-                        } )
-                    });
-
-            if (events.length === 0) {
-                return null
-            }
-
-            days = Object.keys(days);
-            return {
-                type: Provider.ItemType.TVSHOW,
-                _id: id,
-                imdb_id: 'ccc' + id,
-                tvdb_id: 'ccc-' + data.acronym,
-                title: data.title,
-                genres: ["Event", "Conference"],
-                year: year,
-                poster: img,
-                backdrop: img,
-                slug: data.slug,
-                rating: {
-                    hated: 0,
-                    loved: 0,
-                    votes: 0,
-                    percentage: 0,
-                    watching: 0
-                },
-                num_seasons: days.length,
-                days: days,
-                raw_events: events,
-                last_updated: updated.unix()
-            }
-        })
-};
-
-var formatForButter = function(data) {
-    if (!data) return Promise.reject("No Data !");
-
-    return Promise.all(data.map(formatElementForButter.bind(this)).filter(d => d))
-        .then(function (results) {
-            return {
-                results: results,
-                hasMore: true
-            }
-        })
 }
 
-var generateEventTorrents = function(formats, data) {
-    var recordings = formats.reduce(function (a, f) {
+function getEventDate(event) {
+    if (event.date) {
+        return event.date.split('T')[0];
+    }
+
+    return event.release_date;
+}
+
+function processEvents(events, langs) {
+    let days = {}
+    let result = events
+        .filter((event) => {
+            if (langs) {
+                return langs.indexOf(event.original_language) !== -1
+            }
+
+            return true
+        }).map((event) => {
+            var day = getEventDate(event);
+            days[day] = 1;
+
+            return Object.assign({}, event, {
+                first_aired: event.date ? moment(event.date).unix() : 0,
+                day: day
+            })
+        });
+
+    return {
+        events: result,
+        days: days
+    }
+}
+
+function generateEventTorrents(formats, data) {
+    var recordings = formats.reduce((a, f) => {
         if (a.length !== 0) {
             return a;
         }
 
-        return data.recordings.filter(function (r) {
-            return r.mime_type === f;
-        })
+        return data.recordings.filter((r) => (r.mime_type === f))
     }, [])
 
-    return recordings.reduce(function (a, r) {
-        var quality = Object.keys(Provider.QualityType).map(function(q){
-            return Provider.QualityType[q];
-        }).reduce(function(ret, c) {
-            return (Math.abs(r.height - parseInt(ret)) < Math.abs(r.height - parseInt(c))) ? ret : c;
-        }, 0)
+    return recordings.reduce((a, r) => {
+        var quality = Object.keys(Provider.QualityType)
+                            .map((q) => (Provider.QualityType[q]))
+                            .reduce((ret, c) => {
+                                if (Math.abs(r.height - parseInt(ret, 10)) <
+                                    Math.abs(r.height - parseInt(c, 10))) {
+
+                                    return ret
+                                }
+
+                                return c
+                            }, 0)
 
         a[quality] = {
             size: r.size * 1000000,
-            url: r.recording_url + '.torrent',
+            url: `${r.recording_url}.torrent`,
             peers: 0,
             seeds: 0
         }
+
         return a;
     }, {})
 }
 
-var formatEventForButter = function(formats, event, idx, data) {
+function formatEventForButter(torrents, event, idx) {
     return {
-        torrents: generateEventTorrents(formats, data),
+        torrents: torrents,
         watched: {
-            watched: false,
+            watched: false
         },
         date_based: false,
         first_aired: event.first_aired,
@@ -172,68 +106,178 @@ var formatEventForButter = function(formats, event, idx, data) {
         title: event.title,
         episode: idx,
         season: event.season,
-        tvdb_id: event.slug,
+        tvdb_id: event.slug
+    }
+}
+
+module.exports = class CCC extends Provider {
+    constructor (args, config = defaultConfig) {
+        super(args, config)
+
+        this.urlList = this.args.urlList
+        this.limit = this.args.limit
+        this.axiosConfig = {
+            baseURL: this.urlList[0],
+            strictSSL: false,
+            json: true,
+            timeout: this.args.timeout
+        }
+        this.axios = axios.create(this.axiosConfig)
+
+        debug('created instance with', this.axiosConfig)
     }
 
-}
+    formatElementForButter(data) {
+        var langs = this.args.langs;
 
-function getEventDate(event) {
-    if (event.date)
-        return event.date.split('T')[0];
-    return event.release_date;
-}
+        var id = data.url.split('/').pop();
+        var updated = moment(data.updated_at);
+        var year = updated.year();
+        var img = data.logo_url;
 
-CCC.prototype._formatDetailsForButter = function(data) {
-    var events = data.raw_events;
-    delete data.raw_events;
-    var URL = this.URL;
-    var formats = this.args.formats;
+        return this.axios.request({baseURL: data.url})
+            .then((res) => {
+                let conf = res.data
 
-    var eventPromises = data.days.sort().reduce(function(a, d) {
-        var dayEvents = events.filter(function(e) {
-            return e.day === d
-        }).sort(function(a, b) {
-            return a.first_aired > b.first_aired;
-        }).map(function(event, idx) {
-            var day = getEventDate(event);
-            event.season = data.days.indexOf(day) + 1;
-            return deferRequest(URL + '/events/' + event.guid)
-                .then(function (data) {
-                    return formatEventForButter(formats, event, idx, data)
+                debug('ok, got conf', conf)
+                var {events, days} = processEvents(conf.events, langs)
+                if (events.length === 0) {
+                    return null
+                }
+
+                days = Object.keys(days);
+
+                return {
+                    type: Provider.ItemType.TVSHOW,
+                    _id: id,
+                    imdb_id: `ccc${id}`,
+                    tvdb_id: `ccc-${data.acronym}`,
+                    title: data.title,
+                    genres: [
+                        'Event',
+                        'Conference'
+                    ],
+                    year: year,
+                    poster: img,
+                    backdrop: img,
+                    slug: data.slug,
+                    rating: {
+                        hated: 0,
+                        loved: 0,
+                        votes: 0,
+                        percentage: 0,
+                        watching: 0
+                    },
+                    num_seasons: days.length,
+                    days: days,
+                    raw_events: events,
+                    last_updated: updated.unix()
+                }
+            })
+    }
+
+    formatForButter(data) {
+        if (!data) {
+            return Promise.reject(new Error('No Data !'));
+        }
+
+        return Promise.all(data
+            .slice(this.limit)
+            .map(this.formatElementForButter.bind(this))
+            .filter((d) => (d)))
+                      .then((results) => ({
+                          results: results,
+                          hasMore: true
+                      }))
+    }
+
+    queryTorrents(filters = {}) {
+        let query = '/conferences'
+        var params = {}
+        //var genres = ''
+        params.sort = 'seeds'
+        params.limit = '50'
+
+        if (filters.keywords) {
+            params.keywords = filters.keywords.replace(/\s/g, '% ');
+        }
+
+        if (filters.genre) {
+            /*
+               filters.genres.forEach(function(g) {
+               genres += '&genre[]='+g;
+               });
+               genres = genres.substring(0, genres.length - 1);
+               win.info('genres', genres);
+             */
+            params.genre = filters.genres[0];
+        }
+
+        if (filters.order) {
+            params.order = filters.order;
+        }
+
+        if (filters.sorter && filters.sorter !== 'popularity') {
+            params.sort = filters.sorter;
+        }
+
+        debug('query', query)
+
+        return this.axios.get(query)
+            .then((res) => (res.data.conferences))
+            .catch((err) => {
+                debug('CCC', 'error', err)
+            })
+    }
+
+    formatDetailsForButter(data) {
+        var events = data.raw_events
+        Reflect.deleteProperty(data.raw_events)
+        var URL = this.URL
+        var formats = this.args.formats
+
+        var eventPromises = data.days.sort().reduce((a, d) => {
+            var dayEvents = events
+                .filter((e) => (e.day === d))
+                .sort((a, b) => (a.first_aired > b.first_aired))
+                .map((event, idx) => {
+                    var day = getEventDate(event);
+                    event.season = data.days.indexOf(day) + 1;
+
+                    return this.axios.get(`/events/${event.guid}`)
+                        .then((res) => (formatEventForButter(
+                            generateEventTorrents(formats, res.data),
+                            event, idx
+                        )))
                 })
-        })
 
-        return a.concat(dayEvents)
-    }, [])
+            return a.concat(dayEvents)
+        }, [])
 
-    var updated = moment(data.updated)
-    return Promise.all(eventPromises)
-                  .then(function(events) {
-                      console.log ('GOT EVENTS', events.length)
-                      return {
+        var updated = moment(data.updated)
+
+        return Promise.all(eventPromises)
+                      .then((events) => ({
                           synopsis: data.title,
-                          country: "",
-                          network: "CCC Media",
-                          status: "finished",
+                          country: '',
+                          network: 'CCC Media',
+                          status: 'finished',
                           runtime: 30,
                           last_updated: updated.unix(),
                           __v: 0,
                           episodes: events
-                      }
-                  })
+                      }))
+    }
+
+    // Single element query
+    detail(torrent_id, old_data) {
+        return this.formatDetailsForButter(old_data)
+                   .then((new_data) => (Object.assign(old_data, new_data)))
+    }
+
+    fetch(filters = {}) {
+        return this.queryTorrents(filters)
+                   .then(this.formatForButter.bind(this))
+    }
 }
 
-// Single element query
-CCC.prototype.detail = function (torrent_id, old_data, debug) {
-    return this._formatDetailsForButter.apply(this, [old_data])
-               .then(function (new_data) {
-                   return Object.assign(old_data, new_data)
-               })
-};
-
-CCC.prototype.fetch = function (filters) {
-    return this._queryTorrents.apply(this, [filters || {}])
-        .then(formatForButter.bind(this));
-};
-
-module.exports = CCC;
